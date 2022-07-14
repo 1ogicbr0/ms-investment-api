ee4e8bdd05b6442ca0ee6f955bc08dd15bc232fc
Legoabe, Tebogo <Tebogo.Legoabe@vodacom.co.za>
2022-07-14 21:01:42 +0200

    Remove -ve sign

--- routes/formatAmount.js
+++ routes/formatAmount.js
@@ -5,10 +5,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   console.log('req.params.query --> ', req.query);
 
-  const formattedAmount1 =  parseFloat(req.query.amount1).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount2 =  parseFloat(req.query.amount2).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount3 =  parseFloat(req.query.amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount4 =  parseFloat(req.query.amount4).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const amount1 = parseFloat(req.query.amount1) < 0 ? (-1 * parseFloat(req.query.amount1)) : parseFloat(req.query.amount1);
+  const amount2 = parseFloat(req.query.amount2) < 0 ? (-1 * parseFloat(req.query.amount2)) : parseFloat(req.query.amount2);
+  const amount3 = parseFloat(req.query.amount3) < 0 ? (-1 * parseFloat(req.query.amount3)) : parseFloat(req.query.amount3);
+  const amount4 = parseFloat(req.query.amount4) < 0 ? (-1 * parseFloat(req.query.amount4)) : parseFloat(req.query.amount4);
+  //
+  const formattedAmount1 =  parseFloat(amount1).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const formattedAmount2 =  parseFloat(amount2).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const formattedAmount3 =  parseFloat(amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const formattedAmount4 =  parseFloat(amount4).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
 
   res.status(200).json({formattedAmount1: formattedAmount1, formattedAmount2: formattedAmount2, formattedAmount3: formattedAmount3, formattedAmount4: formattedAmount4});
 });
