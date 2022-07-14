0d1e83987e747ad0a3821c2e3efc26509dd4f0e0
Legoabe, Tebogo <Tebogo.Legoabe@vodacom.co.za>
2022-07-14 20:48:15 +0200

    Fix typo

--- routes/formatAmount.js
+++ routes/formatAmount.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   const formattedAmount1 =  parseFloat(req.query.amount1).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
   const formattedAmount2 =  parseFloat(req.query.amount2).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
   const formattedAmount3 =  parseFloat(req.query.amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount4 =  parseFloat(req.query.amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const formattedAmount4 =  parseFloat(req.query.amount4).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
 
   res.status(200).json({formattedAmount1: formattedAmount1, formattedAmount2: formattedAmount2, formattedAmount3: formattedAmount3, formattedAmount4: formattedAmount4});
 });
