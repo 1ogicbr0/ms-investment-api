77504314b96e1633905088f948f97baada35bdee
Legoabe, Tebogo <Tebogo.Legoabe@vodacom.co.za>
2022-07-16 18:20:07 +0200

    Remove decimals

--- routes/formatAmount.js
+++ routes/formatAmount.js
@@ -10,10 +10,10 @@ router.get('/', function(req, res, next) {
   const amount3 = parseFloat(req.query.amount3) < 0 ? (-1 * parseFloat(req.query.amount3)) : parseFloat(req.query.amount3);
   const amount4 = parseFloat(req.query.amount4) < 0 ? (-1 * parseFloat(req.query.amount4)) : parseFloat(req.query.amount4);
   //
-  const formattedAmount1 =  parseFloat(amount1).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount2 =  parseFloat(amount2).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount3 =  parseFloat(amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
-  const formattedAmount4 =  parseFloat(amount4).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+  const formattedAmount1 =  parseFloat(amount1).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' }).replace(',00','');
+  const formattedAmount2 =  parseFloat(amount2).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' }).replace(',00','');
+  const formattedAmount3 =  parseFloat(amount3).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' }).replace(',00','');
+  const formattedAmount4 =  parseFloat(amount4).toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' }).replace(',00','');
 
   res.status(200).json({formattedAmount1: formattedAmount1, formattedAmount2: formattedAmount2, formattedAmount3: formattedAmount3, formattedAmount4: formattedAmount4});
 });
